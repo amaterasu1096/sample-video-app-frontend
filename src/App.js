d6a05b19fa9e5bc4ef7b76495fa5284b7e78e5b1
amaterasu1096 <74552615+amaterasu1096@users.noreply.github.com>
2024-05-24 11:15:05 +0700

    feature 2 home page (#2)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.scss';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import ShareVideo from './pages/ShareVideo'
@@ -12,6 +14,18 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/share" element={<ShareVideo />} />
       </Routes>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </BrowserRouter>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,55 @@
-import logo from '../logo.svg';
+import { useEffect, useState, useRef, useCallback } from 'react';
+import { toast } from 'react-toastify'
+import { fetchVideos, voteVideo } from '../services/api';
+import Video from '../components/Video';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default function Home() {
+  const isAuthenticated = !!localStorage.getItem('token');
+  const [videos, setVideos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    loadVideos(page)
+  }, [page]);
+
+  const loadVideos = async (page) => {
+    try {
+      const { data } = await fetchVideos(page)
+      setVideos((prevVideos) => [...prevVideos, ...data.videos])
+      setHasMore(data.meta.current_page < data.meta.total_pages);
+    } catch (e) {
+      toast(e.response.data.error)
+    }
+  };
+
+  const handleVote = async (videoId, voteType) => {
+    try {
+      const data = await voteVideo(videoId, voteType);
+      setVideos((prevVideos) =>
+        prevVideos.map((video) =>
+          video.id === videoId ? { ...video, vote: data.data.vote_type } : video
+        )
+      );
+      toast(data.data.message)
+    } catch (e) {
+      toast(e.response.data.error)
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='videos'>
+      <InfiniteScroll
+        dataLength={videos.length}
+        next={() => setPage((prevPage) => prevPage + 1)}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+      >
+        {videos.map((video) => (
+          <Video video={video} key={video.id} isAuthenticated={isAuthenticated} onVote={handleVote} />
+        ))}
+      </InfiniteScroll>
     </div>
   );
 }
