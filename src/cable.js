2dfda8c732ff41967c2e888f8eeb229a37c260d3
amaterasu1096 <amaterasu1096@gmail.com>
2024-05-25 11:36:36 +0700

    feature 5 add env and readme

--- src/cable.js
+++ src/cable.js
@@ -2,6 +2,6 @@ import { createConsumer } from '@rails/actioncable';
 
 const CableApp = {};
 const token = localStorage.getItem('token');
-CableApp.cable = createConsumer(`ws://localhost:3001/cable?token=${token}`);
+CableApp.cable = createConsumer(`${process.env.REACT_APP_WEBSOCKET_URL}?token=${token}`);
 
 export default CableApp;
